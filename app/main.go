3046d041c531dfa884cd4f16bbae7474b2b6377c
Alidev11 <karwawiali@gmail.com>
2025-11-10 21:35:36 +0100

    add errors

--- app/main.go
+++ app/main.go
@@ -8,5 +8,9 @@ import (
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
+	var cmd string
 	fmt.Fprint(os.Stdout, "$ ")
+	fmt.Scanln(&cmd)
+	fmt.Fprint(os.Stdout, cmd + ": command not found\n")
+
 }
