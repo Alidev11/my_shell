ed1479b095bbeb02d09ad44897826444dc4e521d
Alidev11 <karwawiali@gmail.com>
2025-11-10 21:43:25 +0100

    add REPL

--- app/main.go
+++ app/main.go
@@ -9,8 +9,11 @@ import (
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	var cmd string
-	fmt.Fprint(os.Stdout, "$ ")
-	fmt.Scanln(&cmd)
-	fmt.Fprint(os.Stdout, cmd + ": command not found\n")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		fmt.Scanln(&cmd)
+		fmt.Fprint(os.Stdout, cmd + ": command not found\n")
+	}
+	
 
 }
