ff3abebaf29cecd942311b4dc769e0423d6d5627
Alidev11 <karwawiali@gmail.com>
2025-11-10 23:20:47 +0100

    add exit command

--- app/main.go
+++ app/main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
-	var cmd string
+	scanner := bufio.NewScanner(os.Stdin)
+	
 	for {
+		// Print $ and read commands until exiting the loop
 		fmt.Fprint(os.Stdout, "$ ")
-		fmt.Scanln(&cmd)
-		fmt.Fprint(os.Stdout, cmd + ": command not found\n")
+		scanner.Scan()
+		cmd := scanner.Text()
+		
+		// Exit command
+		if strings.HasPrefix(cmd, "exit") {
+			parts := strings.Fields(cmd)
+			if(len(parts) == 2) {
+				i, err := strconv.Atoi(parts[1])
+				if err != nil {
+					fmt.Println("Status code is wrong: ", err)
+					continue
+				} else {
+					os.Exit(i)
+				}
+			}
+		} else {
+			fmt.Fprint(os.Stdout, cmd + ": command not found\n")
+		}
 	}
 	
 
